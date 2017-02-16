92744d7fa358ec3b98ab23e6ba35a3e12f7a35f8
gaocaisheng <450214001@qq.com>
2017-02-16 23:03:09 +0800

    remove package name

--- test.bat
+++ test.bat
@@ -5,7 +5,7 @@ adb wait-for-device
 @echo on
 :::::::::::start a new test:::::::::::
 @echo off
-adb shell "monkey -p com.kungeek.android.ftsp.enterprise.pre.release -s 100 --throttle 1000 --randomize-throttle --ignore-crashes --ignore-timeouts -v -v 300000 1>/mnt/sdcard/monkey.txt 2>/mnt/sdcard/error.txt"
+adb shell "monkey -p com.xxxxxxx -s 100 --throttle 1000 --randomize-throttle --ignore-crashes --ignore-timeouts -v -v 300000 1>/mnt/sdcard/monkey.txt 2>/mnt/sdcard/error.txt"
 @echo on
 :::::::::::wait a new phone:::::::::::
 @echo off
